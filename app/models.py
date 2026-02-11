a71a48b397b22e41cb2210d8cd9341685feae5ce
Joker8-h <arlisalfonsopertuzvillareal@gamil.com>
2026-02-11 17:17:19 -0500

    migrar

--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from app.database import Base
 
 
 class User(Base):
-    __tablename__ = 'usuarios'
+    __tablename__ = 'Usuarios'  
+
     idUsuarios = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(100))
     email = Column(String(150), unique=True)
-    fotoPerfil = Column(String(255), nullable=True)  # URL de la imagen en Cloudinary
+    fotoPerfil = Column(String(255), nullable=True)
